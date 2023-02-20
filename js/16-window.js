09e0c5ad644875e6e868df01594472bfb89a0e92
Harsh <harshj7.net@gmail.com>
2023-02-21 01:07:47 +0530

    add all 16 winodw till line no 77 line and add issue.txt till 3 line

--- js/16-window.js
+++ js/16-window.js
@@ -15,6 +15,63 @@ console.log(window.innerWidth); //1280
 
 document.write(window.outerHeight); //680 in px . pura browser ka height
 document.write(window.outerWidth); //1280
-*/
+
 
 //new window kaise open karte hn
+
+let btn = document.getElementById("btn1");
+btn.addEventListener('click', function () {
+    window.open();  //window object ka ek method. btn clcik p ek blank window open ho jata h.
+});
+
+
+
+let btn = document.getElementById("btn1");
+btn.addEventListener('click', function () {
+    window.open(url, target/window_name, windowFeatures);  //esme 3 parameter pass hota h.
+});
+
+
+
+let btn = document.getElementById("btn1");
+let url = "https://www.google.com";
+let features = "height=500,width=500";
+btn.addEventListener('click', function () {
+    window.open(url, 'google', features);  //500x500 ka ek window google.com wala open ho jayega google name se.opened window ka url change nhi kar sakte.
+});
+
+
+
+//name google dene s kya use hota h.
+//hum chahte hn ki google.com jo open h usime open ho yahoo.com btn2 clcik pe.
+
+let btn = document.getElementById("btn1");
+let btns = document.getElementById("btn2");
+let close = document.getElementById("close");
+let url = "https://www.google.com";
+let features = "height=500,width=500";
+btn.addEventListener('click', function () {
+    window.open(url, 'google', features);  //500x500 ka ek window google.com wala open ho jayega google name se.opened window ka url change nhi kar sakte.
+});
+
+btns.addEventListener('click', function () {
+    window.open('https://www.yahoo.com', 'google'); //jo google wala window open h, usime yahoo wala window open ho btn2 click p
+});
+
+*/
+let win;
+let btn = document.getElementById("btn1");
+// let btns = document.getElementById("btn2");
+let close = document.getElementById("close");
+let url = "https://www.google.com";
+let features = "height=500,width=500";
+btn.addEventListener('click', function () {
+    win = window.open(url, 'google', features);
+});
+
+
+
+//popup close
+close.addEventListener('click', function () {
+    win.close(); //Uncaught ReferenceError: win is not defined. so us win ko global bnna padega
+});
