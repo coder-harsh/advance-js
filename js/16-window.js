1a1128d8cfadb2d1445fd0db0c01d11129601278
Harsh <harshj7.net@gmail.com>
2023-02-20 23:50:42 +0530

    add 16-window till line no 20

---
+++ js/16-window.js
@@ -0,0 +1,20 @@
+//ab hum window ke sath thoda play karte hn
+
+
+//ab hum jante hn window ka size pata karna chahte hn
+//2 tarh ka window size hota h. 1. innerheight and innerwidth  2. outerheight and outerwidth
+
+//1.innerheight and innerwidth jahan humare webpage dikhega
+
+//2. outerheight and outerwidth: pura browser
+
+/*
+console.log(window.innerHeight); //259 in px. sirf page ka height
+console.log(window.innerWidth); //1280
+
+
+document.write(window.outerHeight); //680 in px . pura browser ka height
+document.write(window.outerWidth); //1280
+*/
+
+//new window kaise open karte hn
